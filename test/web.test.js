d4590826575a29c597205b93511f1a6f241d6331
Matt Marashi <matt@marashi.dev>
2024-09-01 14:29:47 -0400

    test: configure debugger and test

---
+++ test/web.test.js
@@ -0,0 +1,7 @@
+import { describe, it } from "node:test";
+
+describe("Web app test suite", () => {
+  it("should be able to run tests", () => {
+    console.log("Running tests for web app");
+  });
+});
