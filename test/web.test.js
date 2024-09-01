68390526b14751f877f746a694cb6339a9d7c922
Matt Marashi <matt@marashi.dev>
2024-09-01 16:00:25 -0400

    test: add mocking

--- test/web.test.js
+++ test/web.test.js
@@ -1,7 +1,45 @@
-import { describe, it } from "node:test";
+import { describe, it, before } from "node:test";
+import Controller from "../src/shared/controller.js";
+import View from "../src/platforms/web/view.js";
+
+globalThis.window = {};
+
+function getDocument(mock) {
+  globalThis.alert = mock.fn(() => {});
+  globalThis.document = {
+    querySelector: mock.fn((id) => ({
+      appendChild: mock.fn(() => {}),
+      reset: mock.fn(() => {}),
+      addEventListener: mock.fn((event, fn) => {
+        return fn({
+          preventDefault: mock.fn(() => {}),
+        });
+      }),
+    })),
+    createElement: mock.fn((name) => ({
+      classList: {
+        add: mock.fn(() => {}),
+      },
+    })),
+    addEventListener: mock.fn((event, fn) => {
+      return fn({
+        preventDefault: mock.fn(() => {}),
+      });
+    }),
+  };
+
+  return globalThis.document;
+}
 
 describe("Web app test suite", () => {
-  it("should be able to run tests", () => {
+  let _controller;
+  before(() => {
+    // _controller = Controller.init({ view: new View() });
+  });
+  it("should be able to run tests", (context) => {
+    const document = getDocument(context.mock);
+    _controller = Controller.init({ view: new View() });
+
     console.log("Running tests for web app");
   });
 });
